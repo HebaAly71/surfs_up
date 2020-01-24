07120c8a3e487a1ca2031ff15873306db4f08ab3
HebaAly71 <47098642+HebaAly71@users.noreply.github.com>
2020-01-23 23:14:33 -0800

    updated

--- climaste_analysis.py
+++ climaste_analysis.py
@@ -59,19 +59,29 @@ session = Session(engine)
 #Starting from the last data point in the database. 
 
 # Calculate the date one year from the last date in data set.
-
+prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
 # Perform a query to retrieve the data and precipitation scores
-
+results = []
+results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= prev_year).all()
+print(results)
+# %%
 # Save the query results as a Pandas DataFrame and set the index to the date column
-
+df = pd.DataFrame(results, columns=['date','precipitation'])
+df.set_index(df['date'], inplace=True)
+print(df.to_string(index=False))
+#print(df)
+# %%
 # Sort the dataframe by date
-
+df = df.sort_index()
+# %%
+print(df.to_string(index=False))
+# %%
 # Use Pandas Plotting with Matplotlib to plot the data
-
+df.plot()
 
 # %%
 # Use Pandas to calcualte the summary statistics for the precipitation data
-
+df.describe()
 
 # %%
 # How many stations are available in this dataset?
